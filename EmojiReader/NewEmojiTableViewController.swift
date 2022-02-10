e23f7a2045ace7147dedecf5f00ddde4ac6e08b9
OzzMO94 <adinovaaa@yandex.ru>
2022-02-11 00:59:00 +0300

    work with code

--- EmojiReader/NewEmojiTableViewController.swift
+++ EmojiReader/NewEmojiTableViewController.swift
@@ -8,6 +8,9 @@
 import UIKit
 
 class NewEmojiTableViewController: UITableViewController {
+    
+    var emoji = Emoji(emoji: "", name: "", description: "", isFavorite: false)
+    
     @IBOutlet weak var emojiTF: UITextField!
     @IBOutlet weak var nameTF: UITextField!
     @IBOutlet weak var descriptionTF: UITextField!
@@ -15,9 +18,29 @@ class NewEmojiTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-    }
-    @IBAction func textChange(_ sender: UITextField) {
+        updateSaveButtonState()
     }
     
+    private func updateSaveButtonState() {
+        let emojiText = emojiTF.text ?? ""
+        let nameText = nameTF.text ?? ""
+        let descriptionText = descriptionTF.text ?? ""
+        
+        saveButton.isEnabled = !emojiText.isEmpty && !nameText.isEmpty && !descriptionText.isEmpty
+    }
+    
+    @IBAction func textChange(_ sender: UITextField) {
+        updateSaveButtonState()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        guard segue.identifier == "saveSegue" else {return}
+        let emoji = emojiTF.text ?? ""
+        let name = nameTF.text ?? ""
+        let description = descriptionTF.text ?? ""
+        
+        self.emoji = Emoji(emoji: emoji, name: name, description: description, isFavorite: self.emoji.isFavorite)
+    }
+
 }
